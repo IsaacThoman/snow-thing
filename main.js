d29a353eff61537078ce6ecdd3d050cd254c1a2a
isaacthoman <isaacthoman@outlook.com>
2021-12-07 22:14:20 -0500

    messy calibration work

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ const mThreshold = 100;
 const camWidth = 640;
 const camHeight = 360;
 
+var debugOutputType = 1;
+
 var lastImg = ctx2.getImageData(0,0,camWidth,camHeight);
 
 
@@ -24,7 +26,7 @@ function difference(img1, img2){ //gets image difference, returns as image.data
 
 let img1Data = img1.data;
 let img2Data = img2.data;
-let newImgData = img1.data;
+let newImgData = img1.data.slice();
 let imgLength = img1Data.length;
 
     for(let i = 0; i<imgLength; i+=4){
@@ -72,7 +74,13 @@ doFrame = window.setInterval(function(){
 
     let diffImgData = difference(thisImg, lastImg);
 //    console.log(diffImgData)
-    ctx.putImageData(diffImgData,0,0);
+    if(debugOutputType == 0){
+      //  ctx.putImageData(diffImgData,0,0);
+    }
+    if(debugOutputType == 1){
+        ctx.putImageData(thisImg,0,0);
+    }
+
 
     let motionCenter = getCenter(diffImgData,mThreshold,camWidth,camHeight);
 
@@ -127,9 +135,11 @@ let snowflakeDirection = [];
 let snowflakeSpeed = [];
 let snowflakeRadius = [];
 let snowflakeTimeLeft = [];
-
+var gettingBounds = false;
 
 gameFrame = window.setInterval(function(){
+    if(gettingBounds){
+        return;}
     gameCtx.drawImage(snowBg,0,0,640,360)
 
 for(let i = 0; i<snowflakeX.length; i++){
@@ -137,7 +147,7 @@ for(let i = 0; i<snowflakeX.length; i++){
 
     snowflakeX[i]+= Math.cos(snowflakeDirection[i])*snowflakeSpeed[i];
     snowflakeY[i]+= Math.sin(snowflakeDirection[i])*snowflakeSpeed[i];
-    console.log([snowflakeX[i],snowflakeY[i]])
+ //   console.log([snowflakeX[i],snowflakeY[i]])
    snowflakeTimeLeft[i]--;
     snowflakeSpeed[i]/=1.1;
 
@@ -173,4 +183,71 @@ function makeSnowflake(x,y,dir,speed,radius,lifetime){
     snowflakeRadius.push(radius);
     snowflakeTimeLeft.push(lifetime);
 }
+var boundsFound = [];
 
+function getBoundsStart(){
+    gettingBounds = true;
+fOn = 0;
+for(let cornerOn = 0; cornerOn<4; cornerOn++){
+    doFlashes(cornerOn)
+}
+
+
+
+}
+
+function getBoundsPt2(cornerOn){
+    let boundX = 0;
+    let boundY = 0;
+    let samples = 0;
+    for(let i = 0; i<centersTracked.length; i++){
+        if(!isNaN(centersTracked[i][0])){
+            boundX+=centersTracked[i][0];
+            boundY+=centersTracked[i][1];
+            samples++;
+        }
+    }
+    boundX/=samples;
+    boundY/=samples;
+    getBoundsStart[cornerOn] = [boundX,boundY];
+}
+
+
+
+var fOn = 0;
+function doFlashes(cornerOn) {
+
+
+    fOn++;
+    gameCtx.drawImage(snowBg,0,0,640,360)
+    gameCtx.beginPath();
+    gameCtx.fillStyle = "#0000ff"
+    if(fOn%3==0){
+        gameCtx.fillStyle = "#ff0000"
+    }else if(fOn%3==1){
+        gameCtx.fillStyle = "#00ff00"
+    }
+    if(cornerOn==0)
+        gameCtx.rect(5,5,15,15);
+    if(cornerOn==1)
+        gameCtx.rect(camWidth-20,5,15,15);
+    if(cornerOn==2)
+        gameCtx.rect(5,5,15,15);
+    if(cornerOn==3)
+        gameCtx.rect(5,5,15,15);
+
+    gameCtx.fill();
+    gameCtx.closePath();
+    if(fOn<25) {
+        setTimeout(function () {
+            doFlashes(cornerOn)
+        }, 150);
+    }else{
+        getBoundsPt2(cornerOn)
+        fOn = 0;
+        setTimeout(function () {
+            doFlashes(cornerOn)
+        }, 3000);
+    }
+
+}
